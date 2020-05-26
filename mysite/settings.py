700901868a061b65435ae41156333c09be33f0b5
Pudding <g17914hr@gm.tsuda.ac.jp>
2020-05-27 02:12:20 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,4 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/puddingdog/djangogirls/blog/static'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
